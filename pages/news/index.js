9b8fdb8010f561a2eb91a807736e587bc69a263f
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-18 21:03:29 -0500

    Updating to Link native component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,21 +1,23 @@
+import Link from "next/link";
+
 function News() {
   return (
     <>
       <h1>The News Page</h1>
       <ul>
         <li>
-          <a href="/news/nextjs-is-a-great-framework">
+          <Link href="/news/nextjs-is-a-great-framework">
             NextJs is a great framework
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/news/something-else">Something else</a>
+          <Link href="/news/something-else">Something else</Link>
         </li>
         <li>
-          <a href="/news/third-link">Third Link</a>
+          <Link href="/news/third-link">Third Link</Link>
         </li>
         <li>
-          <a href="/news/last_new">Last new</a>
+          <Link href="/news/last_new">Last new</Link>
         </li>
       </ul>
     </>
